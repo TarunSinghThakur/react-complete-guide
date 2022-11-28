90b7d1802587ccbb7ad9bd1edaefc50ac202858f
Tarun Thakur <singhbutes@gmail.com>
2022-11-28 16:23:24 +0530

    Added card css as common

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from './Card';
 
 const ExpenseItem = ({ title, amount, date }) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date}/>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">INR {amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
