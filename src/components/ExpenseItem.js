e385b14ff1634101980702706ef51ebd9504274f
Tarun Thakur <singhbutes@gmail.com>
2022-12-01 16:16:03 +0530

    Dynamic button click update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,24 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from './Card';
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [myTitle, setTitle] = useState(title);
+
+  const clickHandler = () => {
+    setTitle("I am updated!");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{myTitle}</h2>
         <div className="expense-item__price">INR {amount}</div>
       </div>
+      <button onClick={clickHandler}>ClickMe</button>
     </Card>
   );
 };
