df42ba3874c63724b7ee5026af635a778c15af17
Tarun Thakur <singhbutes@gmail.com>
2022-12-15 10:32:55 +0530

    Added dynamic input from form

--- src/App.js
+++ src/App.js
@@ -7,32 +7,13 @@ import ExpensesFilter from './components/ExpenseFilter/ExpensesFilter';
 import Card from './components/Card';
 
 function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const [filterValue, setFilterValue] = useState('2020');
 
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
   const addExpenseHandler = expense => {
-    console.log(expense, 'This is expense')
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    })
   }
   
   const filterHandler = selectedFilter => {
@@ -55,5 +36,32 @@ function App() {
     </div>
   );
 }
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 export default App;
