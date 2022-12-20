e12f17a8595c03654ffb9c374534724aaaaa1692
Tarun Thakur <singhbutes@gmail.com>
2022-12-20 17:54:06 +0530

    Added filter chart

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -47,7 +47,7 @@ const ExpenseForm = ({ onSaveExpenseData, onCancel }) => {
         event.preventDefault();
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,36 +1,41 @@
 import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
-import './NewExpense.css';
+import "./NewExpense.css";
 
 const NewExpense = ({ onAddExpense }) => {
-    const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            id: Math.random().toString(),
-            ...enteredExpenseData
-        }
-        onAddExpense(expenseData);
-        setIsEditing(false);
-    }
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      id: Math.random().toString(),
+      ...enteredExpenseData,
+    };
+    onAddExpense(expenseData);
+    setIsEditing(false);
+  };
 
-    const startEditingHandler = () => {
-        setIsEditing(true);
-    }
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
 
-    const stopEditingHandler = () => {
-        setIsEditing(false);
-    }
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
 
-    return (
-        <div className="new-expense">
-            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
-            {isEditing && <ExpenseForm
-                onSaveExpenseData={saveExpenseDataHandler}
-                onCancel={stopEditingHandler} />}
-        </div>
-    )
-}
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
+};
 
 export default NewExpense;
