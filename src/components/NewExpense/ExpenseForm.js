9e49932de582c6d95067d5feb0eeec287c2cbea5
Tarun Thakur <singhbutes@gmail.com>
2022-12-20 13:02:44 +0530

    Added conditional cancel and Add new expense button

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import './ExpenseForm.css';
 
-const ExpenseForm = ({ onSaveExpenseData }) => {
+const ExpenseForm = ({ onSaveExpenseData, onCancel }) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState(0);
     const [enteredDate, setEnteredDate] = useState('');
@@ -56,6 +56,7 @@ const ExpenseForm = ({ onSaveExpenseData }) => {
         setEnteredTitle('');
         setEnteredAmount(0);
         setEnteredDate('');
+        
     };
 
     return (
@@ -78,6 +79,7 @@ const ExpenseForm = ({ onSaveExpenseData }) => {
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={onCancel}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
